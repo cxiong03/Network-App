1860fb5cd8a1525bee2ab0b573de5262bd384e4d
Chang <cnxiong03@gmail.com>
2020-07-11 17:46:16 -0400

    adding education and experiences done

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -31,8 +31,7 @@ const EditProfile = ({ profile: { profile, loading}, createProfile, getCurrentPr
             location: loading || !profile.location ? '' : profile.location,
             status: loading || !profile.status ? '' : profile.status,
             skills: loading || !profile.skills ? '' : profile.skills.join(','),
-            githubusername:
-                loading || !profile.githubusername ? '' : profile.githubusername,
+            githubusername: loading || !profile.githubusername ? '' : profile.githubusername,
             bio: loading || !profile.bio ? '' : profile.bio,  
             twitter: loading || !profile.social ? '' : profile.social.twitter,  
             facebook: loading || !profile.social ? '' : profile.social.facebook,  
