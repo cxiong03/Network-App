589e1f746c3b9900f4cc51fa3f7200dd39c569ee
Chang <cnxiong03@gmail.com>
2020-07-12 12:00:23 -0400

    Delete education, experiences, account, and the post of deleted account done

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,9 +6,9 @@ import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
 import Experience from './Experience';
 import Education from './Education';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
-const Dashboard = ({ getCurrentProfile, auth: {user}, profile: { profile, loading} }) => {
+const Dashboard = ({ getCurrentProfile, deleteAccount, auth: {user}, profile: { profile, loading} }) => {
     useEffect(() => {
         getCurrentProfile();
     }, []);
@@ -21,6 +21,12 @@ const Dashboard = ({ getCurrentProfile, auth: {user}, profile: { profile, loadin
             <DashboardActions />
             <Experience experience={profile.experience} />
             <Education education={profile.education} />
+
+            <div className="my-2">
+                <button className="btn btn-danger" onClick={() => deleteAccount()}>
+                    <i className="fas fa-user-minus"></i> Delete My Account
+                </button>
+            </div>
         </Fragment>) : (<Fragment>
             <p>You have not yet setup a profile, please add some info</p>
             <Link to='/create-profile' className='btn btn-primary my-1'>
@@ -34,6 +40,7 @@ const Dashboard = ({ getCurrentProfile, auth: {user}, profile: { profile, loadin
 
 Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
+    deleteAccount: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     profile: PropTypes.object.isRequired
 };
@@ -43,6 +50,6 @@ const mapStateToProp = state => ({
     profile: state.profile
 });
 
-export default connect(mapStateToProp, { getCurrentProfile })(Dashboard)
+export default connect(mapStateToProp, { getCurrentProfile, deleteAccount })(Dashboard)
 
 
